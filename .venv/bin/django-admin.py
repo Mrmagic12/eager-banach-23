8fd42b06ca81834b00d443668ab4b59f8dc30ec7
Mrmagic12 <157940462+Mrmagic12@users.noreply.github.com>
2024-03-18 15:46:39 +0000

    added

--- .venv/bin/django-admin.py
+++
@@ -1,4 +0,0 @@
-#!/workspaces/workspace/.venv/bin/python
-# EASY-INSTALL-SCRIPT: 'Django==2.2.28','django-admin.py'
-__requires__ = 'Django==2.2.28'
-__import__('pkg_resources').run_script('Django==2.2.28', 'django-admin.py')
